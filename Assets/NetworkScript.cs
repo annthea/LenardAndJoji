592333ec87c838c2398f814253dd02c494e80477
FusTaFah <mfadel_1990@hotmail.com>
2017-11-19 01:08:52 +0000

    adds network test area

---
+++ Assets/NetworkScript.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class NetworkScript : MonoBehaviour {
+
+    List<MatchInfoSnapshot> matchList = new List<MatchInfoSnapshot>();
+    bool matchCreated;
+    NetworkMatch networkMatch;
+
+    private void Awake()
+    {
+        networkMatch = gameObject.AddComponent<NetworkMatch>();
+    }
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Create Room"))
+        {
+            string matchName = "room";
+            uint matchSize = 2;
+            bool matchAdvertise = true;
+            string matchPassword = "";
+
+            networkMatch.CreateMatch(matchName, matchSize, matchAdvertise, matchPassword, "", "", 0, 0, OnMatchCreate);
+        }
+
+        if (GUILayout.Button("Refresh"))
+        {
+            networkMatch.ListMatches(0, 20, "", true, 0, 0, OnMatchList);
+        }
+
+        if(matchList.Count > 0)
+        {
+            GUILayout.Label("Available rooms");
+        }
+        foreach(MatchInfoSnapshot mis in matchList)
+        {
+            if (GUILayout.Button(mis.name))
+            {
+                networkMatch.JoinMatch(mis.networkId, "", "", "", 0, 0, OnMatchJoined);
+            }
+        }
+    }
+
+    public void OnMatchCreate(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        if (success)
+        {
+            Debug.Log("Create match succeeded");
+            matchCreated = true;
+            NetworkServer.Listen(matchInfo, 9000);
+            Utility.SetAccessTokenForNetwork(matchInfo.networkId, matchInfo.accessToken);
+        }
+        else
+        {
+            Debug.LogError("Create match failed: " + extendedInfo);
+        }
+    }
+
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+    {
+        if (success)
+        {
+            //networkMatch.JoinMatch(matches[0].networkId, "", "", "", 0, 0, OnMatchJoined);
+            matchList = matches;
+        }
+        else
+        {
+            Debug.LogError("List match failed: " + extendedInfo);
+        }
+    }
+
+    public void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        if (success)
+        {
+            Debug.Log("Join match succeeded");
+            if (matchCreated)
+            {
+                Debug.LogWarning("Match already set up, aborting...");
+            }
+            Utility.SetAccessTokenForNetwork(matchInfo.networkId, matchInfo.accessToken);
+            NetworkClient myClient = new NetworkClient();
+            myClient.RegisterHandler(MsgType.Connect, OnConnected);
+            myClient.Connect(matchInfo);
+        }
+        else
+        {
+            Debug.LogError("Join match failed: " + extendedInfo);
+        }
+    }
+
+    public void OnConnected(NetworkMessage msg)
+    {
+        Debug.Log("Connected!");
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PlayerNetworkBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerNetworkBehaviour : NetworkBehaviour {
+
+    bool hasCollided = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        else
+        {
+            keyboardBehaviour();
+        }
+    }
+
+    public bool GetHasCollided()
+    {
+        return hasCollided;
+    }
+
+
+    void keyboardBehaviour()
+    {
+        Vector3 v = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector2 playerPos = v;
+        Collider2D hitCollider = Physics2D.OverlapPoint(playerPos);
+
+        if (hitCollider)
+        {
+            if (hitCollider.gameObject.transform.tag.Equals("Enemy"))
+                if (Input.GetKeyDown("up"))
+                {
+                    Debug.Log("hit");
+                    Destroy(hitCollider.gameObject);
+                }
+        }
+    }
+}
